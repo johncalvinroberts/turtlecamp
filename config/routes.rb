10f995ba91c5b44af74645827c2de4a31dffda30
John Roberts <john.calvin.roberts@gmail.com>
2017-03-29 00:07:26 +0800

    calling it a night. didn't make any changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :students
   root to: 'pages#home'
 
+  get "/dashboard", to: "pages#dashboard"
+  get "/students", to: "pages#counselor_multi_student"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
