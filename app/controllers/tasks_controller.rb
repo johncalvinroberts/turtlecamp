5fc47fc37e3ce58ce455f51e2a61fc9f6c40fcbc
Allen Sanchez <allenjsanchez@gmail.com>
2017-03-29 17:49:07 +0800

    added tasks controller and updated routes from applications to college_apps

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,35 @@
 class TasksController < ApplicationController
   def new
+    @task = Task.new
   end
 
   def create
+    @college_app = CollegeApp.find(params[:college_app_id])
+    @task = @college_app.tasks.new(task_params)
+    if @task.save
+      redirect_to college_apps
+    else
+      render :new
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    @task.save
+    redirect_to college_apps
+  end
+
+  private
+
+  def task_params
+    params.require(:task)
+          .permit(:name,
+                  :due_date,
+                  :status,
+                  :has_attachment)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,10 @@
 Rails.application.routes.draw do
-  get 'tasks/new'
-
-  get 'tasks/create'
-
-  get 'tasks/edit'
-
-  get 'tasks/update'
-
-  get 'tasksnew/create'
-
-  get 'tasksnew/edit'
-
-  get 'tasksnew/update'
-
   root to: 'pages#home'
 
   get "/dashboard", to: "pages#dashboard"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students , only: [:show, :create, :new, :index] do
-    resources :applications, only: [:new, :show, :create] do
+  resources :students, only: [:show, :create, :new, :index] do
+    resources :college_apps, only: [:new, :show, :create] do
       resources :tasks, only: [:new, :create, :edit, :update] do
         member do
           post "/attach_file", to: "tasks#attach_file"
@@ -26,7 +12,7 @@ Rails.application.routes.draw do
       end
     end
   end
-  resources :applications, only: [:show, :create, :new, :index] do
+  resources :college_apps, only: [:show, :create, :new, :index] do
     resources :tasks, only: [:new, :create, :edit, :update] do
       member do
         post "/attach_file", to: "tasks#attach_file"
