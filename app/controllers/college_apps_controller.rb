47af1ffd09cbd871a61dac6b507845d7ebbf4895
John Roberts <john.calvin.roberts@gmail.com>
2017-03-30 02:10:57 +0800

    now kind of sad:

--- app/controllers/college_apps_controller.rb
+++ app/controllers/college_apps_controller.rb
@@ -3,17 +3,27 @@ class CollegeAppsController < ApplicationController
   def new
     @college_app = CollegeApp.new
     @colleges = College.all
+     if current_user.is_counselor
+      @student = User.find_by(counselor_ref: current_user.id, id: params[:id])
+    else
+      @student = current_user.id
+    end
+    raise
   end
 
   def create
-    if current_user.is_counselor
-      @student = User.find_by(counselor_ref: current_user.id, id: params[:id])
-    else
-      @student = current_user
-    end
+    # if current_user.is_counselor
+    #   @student = User.find_by(counselor_ref: current_user.id, id: params[:id])
+    # else
+    #   @student = current_user.id
+    # end
+    # if current_user.is_counselor
+    # @student = User.find_by(counselor_ref: current_user.id)
+    # else @student = current_user
+    # end
     @college_app = CollegeApp.new(college_app_params)
-    @college_app.user_id = @student.id
-    if @college_app.save
+    @college_app.user_id = @student
+    if @college_app.save!
       redirect_to root_path
       # redirect_to student_college_app_path(@student)
     else
@@ -31,7 +41,7 @@ class CollegeAppsController < ApplicationController
 
   private
   def college_app_params
-    params.require(:college_app).permit(:college_id, :deadline, :category, )
+    params.require(:college_app).permit(:college_id, :deadline, :category)
   end
 
 end
