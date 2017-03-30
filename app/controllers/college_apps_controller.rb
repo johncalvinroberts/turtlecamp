1f7559d7ad96e9f635dfec3dc3d6aad24e853137
John Roberts <john.calvin.roberts@gmail.com>
2017-03-30 18:43:03 +0800

    working on update for college apps for student. in this commit, counselors can update and delete apps with no problem

--- app/controllers/college_apps_controller.rb
+++ app/controllers/college_apps_controller.rb
@@ -1,5 +1,5 @@
 class CollegeAppsController < ApplicationController
-
+  before_action :find_app, only: [:show, :edit, :update, :destroy]
   def new
     @college_app = CollegeApp.new
     @colleges = College.all
@@ -11,6 +11,7 @@ class CollegeAppsController < ApplicationController
   end
 
   def create
+     @colleges = College.all
     @college_app = CollegeApp.new(college_app_params)
     if current_user.is_counselor
       @student = User.find_by(counselor_ref: current_user.id, id: params[:student_id])
@@ -33,9 +34,41 @@ class CollegeAppsController < ApplicationController
   def show
   end
 
+  def edit
+    if current_user.is_counselor
+      @student = User.find_by(counselor_ref: current_user.id, id: params[:student_id])
+    else
+      @student = current_user
+    end
+  end
+
+  def update
+    if current_user.is_counselor
+      @student = User.find_by(counselor_ref: current_user.id, id: params[:college_app][:student_id])
+    else
+      @student = current_user
+    end
+    @college_app.update(college_app_params)
+
+    if current_user.is_counselor
+      redirect_to student_college_app_path(@student, @college_app)
+    else
+      redirect_to college_app_path(@student)
+    end
+  end
+
+  def destroy
+    @college_app.destroy
+    redirect_to college_apps_path(@student)
+  end
+
   private
   def college_app_params
     params.require(:college_app).permit(:college_id, :deadline, :category)
+  end
+
+  def find_app
+    @college_app = CollegeApp.find(params[:id])
   end
 
 end
--- app/models/college_app.rb
+++ app/models/college_app.rb
@@ -2,4 +2,5 @@ class CollegeApp < ApplicationRecord
   belongs_to :user
   has_many :tasks
   belongs_to :college
+  validates :college_id, uniqueness: {scope: :user_id, message: "One college at a time"}
 end
