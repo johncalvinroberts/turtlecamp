97a9b3a9d1643cb2dd64257241289eb59e381097
Stephane Delgado <stephanedelgado@gmail.com>
2017-04-03 17:58:35 +0800

    added emblems to colleges on the counselor page and it\s looking good

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-College.destroy_all
+# College.destroy_all
 
 College.new({name:"Amherst", address:"Cambridge, MA", emblem: "Amherst_College.png"}).save!
 College.new({name:"Yale", address:"New Haven, CT", emblem: "Yale_University.png"}).save!
@@ -18,5 +18,3 @@ College.new({name:"Claremont McKenna", address:"Cambridge, MA", emblem: "Claremo
 College.new({name:"Duke", address:"New Haven, CT", emblem: "Duke_University.png"}).save!
 College.new({name:"MIT", address:"14 Old Chapel Rd, Middlebury, VT", emblem: "Massachusetts_Institute_of_Technology.png"}).save!
 College.new({name:"Stanford", address:"Cambridge, MA", emblem: "Stanford_University.png"}).save!
-
-
